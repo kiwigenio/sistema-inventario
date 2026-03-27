96bcb380527aa5fefb213cf0925abba06264ef0a
Kiwigenio <gumballwaterson333@gmail.com>
2026-03-27 10:21:12 -0500

    configuracion final de como es el archivo para que se comunique con la base de datos y no se borre cada dia

--- inventario_web/settings.py
+++ inventario_web/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +80,11 @@ WSGI_APPLICATION = 'inventario_web.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True # Esto es vital para bases de datos profesionales
+    )
 }
 
 
@@ -138,5 +142,6 @@ EMAIL_HOST_USER = 'empresa.comercio.e@gmail.com'
 
 EMAIL_HOST_PASSWORD = 'coohugsvtdontknr'
 
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
